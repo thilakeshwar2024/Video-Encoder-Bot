33cd6945fdc21618f8f04b00cafea6fdd00d3f8c
Zoro3823 <143424322+Zoro3823@users.noreply.github.com>
2023-09-18 06:17:43 +0530

    Update ffmpeg.py

--- VideoEncoder/utils/ffmpeg.py
+++ VideoEncoder/utils/ffmpeg.py
@@ -187,7 +187,7 @@ async def encode(filepath, message, msg):
     # Metadata Watermark
     m = await db.get_metadata_w(message.from_user.id)
     if m:
-        metadata = '-metadata title=ToonEncodes.one -metadata:s:v title=ToonEncodes.one -metadata:s:a title=ToonEncodes.one'
+        metadata = '-metadata title=ToonEncodes.one -metadata:s:v title=ToonEncodes.one -metadata:s:a title=ToonEncodes.one -metadata:s:s title=Encoded By ToonEncodes.one'
     else:
         metadata = ''
 
